c2be436be58943d2964b4aad259f08eaf2f6639d
HemaP2006 <patilhema2006@gmail.com>
2026-03-29 11:05:56 +0530

    Java login Program

---
+++ login.java
@@ -0,0 +1,11 @@
+public class login{
+  public static void main(String[]  args){
+    String username="admin";
+    String password="1234";
+    if(username.equals("admin")&&password.equals("1234")){
+      System.out.println("Login Successfull");
+    }
+    else
+      System.out.println("Login failed")
+        }
+}
